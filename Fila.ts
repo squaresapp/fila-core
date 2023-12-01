0499bc14be1bfdea38ea341636dedc9297c4fa1c
paulgo <spam@heropaul.com>
2023-11-30 19:30:44 -0500

    Preparation for npm

--- Fila.ts
+++ Fila.ts
@@ -610,5 +610,15 @@ namespace Fila
 	}
 }
 
-//@ts-ignore
+//@ts-ignore CommonJS compatibility
 typeof module === "object" && Object.assign(module.exports, { Fila });
+
+// ES module compatibility
+declare module "@scrollapp/fila-core"
+{
+	const __export: { Fila: typeof Fila };
+	export = __export;
+}
+
+// The comment and + prefix is removed during npm run bundle
+//+ export { Fila }
